f81385e364dbab947d8a3b114f2aba84cf5c98a0
maaz1996 <maaz.abdullah111@gmail.com>
2020-07-13 00:15:45 +0530

    feat:weather for a city added

--- src/App_Component/weather.component.jsx
+++ src/App_Component/weather.component.jsx
@@ -8,8 +8,19 @@ const Weather = () => {
           <i className="wi wi-day-sunny display-1"></i>
         </h5>
         <h1 className="py-2">25&deg;</h1>
+        {minmaxTemp(19, 25)}
+        <h4 className="py-3">Slow Rain</h4>
       </div>
     </div>
   );
 };
+
+function minmaxTemp(min, max) {
+  return (
+    <h3>
+      <span className="px-4">{min}&deg;</span>
+      <span className="px-4">{max}&deg;</span>
+    </h3>
+  );
+}
 export default Weather;
