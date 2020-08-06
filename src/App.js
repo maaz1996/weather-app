9f2e767622ecee1e07a3fdf617a654f26031d6f0
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-06 23:47:34 +0530

    feat: api_key and api call

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import Weather from "./App_Component/weather.component";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "weather-icons/css/weather-icons.css";
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+
+const API_key = "";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  getWeather = async () => {
+    const api_call = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=London&appid=${API_key}`
+    );
+    console.log("api_call");
+    const response = await api_call.json();
+    console.log(response);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
