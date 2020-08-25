5bbc20267d551066cc83dc8d0664ff406404e837
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-25 10:59:30 +0530

    get temp function added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,23 @@ const API_key = "";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false,
+    };
     this.getWeather();
+  }
+
+  getCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
   }
   getWeather = async () => {
     const api_call = await fetch(
@@ -18,12 +33,27 @@ class App extends React.Component {
 
     const response = await api_call.json();
     console.log(response);
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      celsius: this.getCelsius(response.main.temp),
+      temp_max: this.getCelsius(response.main.temp_max),
+      temp_min: this.getCelsius(response.main.temp_min),
+      description: response.weather[0].description,
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <Weather />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+        />
       </div>
     );
   }
