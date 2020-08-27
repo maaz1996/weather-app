d48b4f7a5032dc2d1ea0556b6038d6244dd48132
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-28 02:04:42 +0530

    feat: dynamic city,country api call

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
       description: "",
       error: false,
     };
-    this.getWeather();
+
     this.weatherIcon = {
       Thunderstorm: "wi-thunderstorm",
       Drizzle: "wi-sleet",
@@ -63,28 +63,34 @@ class App extends React.Component {
     let cell = Math.floor(temp - 273.15);
     return cell;
   }
-  getWeather = async () => {
-    const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
-    );
-
-    const response = await api_call.json();
-    console.log(response);
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      celsius: this.getCelsius(response.main.temp),
-      temp_max: this.getCelsius(response.main.temp_max),
-      temp_min: this.getCelsius(response.main.temp_min),
-      description: response.weather[0].description,
-    });
-    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    if (city && country) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
+      );
+      const response = await api_call.json();
+      console.log(response);
+      this.setState({
+        city: response.name,
+        country: response.sys.country,
+        celsius: this.getCelsius(response.main.temp),
+        temp_max: this.getCelsius(response.main.temp_max),
+        temp_min: this.getCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+      });
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+    } else {
+      this.setState({ error: true });
+    }
   };
 
   render() {
     return (
       <div className="App">
-        <Form />
+        <Form loadweather={this.getWeather} />
         <Weather
           city={this.state.city}
           country={this.state.country}
--- src/App_Component/form.component.jsx
+++ src/App_Component/form.component.jsx
@@ -1,29 +1,34 @@
 import React from "react";
+import "./form.style.css";
 
 const Form = (props) => {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-3 offset-md-2 ">
-          <input
-            type="text"
-            className="form-control"
-            name="city"
-            autoComplete="off"
-          />
+      <form onSubmit={props.loadweather}>
+        <div className="row">
+          <div className="col-md-3 offset-md-2 ">
+            <input
+              type="text"
+              className="form-control"
+              name="city"
+              autoComplete="off"
+              placeholder="City"
+            />
+          </div>
+          <div className="col-md-3">
+            <input
+              type="text"
+              className="form-control"
+              name="country"
+              autoComplete="off"
+              placeholder="Country"
+            />
+          </div>
+          <div className="col-md-3 mt-md-0 text-md-left">
+            <button className="btn btn-warning">Get Weather</button>
+          </div>
         </div>
-        <div className="col-md-3">
-          <input
-            type="text"
-            className="form-control"
-            name="country"
-            autoComplete="off"
-          />
-        </div>
-        <div className="col-md-3 mt-md-0 text-md-left">
-          <button className="btn btn-warning">Get Weather</button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
