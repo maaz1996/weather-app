51c73a4f90213b602332c1dde1de82ad2c693292
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-28 01:35:49 +0530

    feat: dynamic icon selection

--- src/App.js
+++ src/App.js
@@ -22,9 +22,42 @@ class App extends React.Component {
     this.getWeather();
     this.weatherIcon = {
       Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Cloud: "wi-day-fog",
     };
   }
 
+  get_WeatherIcon(icons, rangeID) {
+    switch (true) {
+      case rangeID >= 200 && rangeID <= 232:
+        this.setState({ icon: this.weatherIcon.Thunderstorm });
+        break;
+      case rangeID >= 300 && rangeID <= 321:
+        this.setState({ icon: this.weatherIcon.Drizzle });
+        break;
+      case rangeID >= 500 && rangeID <= 531:
+        this.setState({ icon: this.weatherIcon.Rain });
+        break;
+      case rangeID >= 600 && rangeID <= 622:
+        this.setState({ icon: this.weatherIcon.Snow });
+        break;
+      case rangeID >= 701 && rangeID <= 781:
+        this.setState({ icon: this.weatherIcon.Atmosphere });
+        break;
+      case rangeID === 800:
+        this.setState({ icon: this.weatherIcon.Clear });
+        break;
+      case rangeID >= 801 && rangeID <= 804:
+        this.setState({ icon: this.weatherIcon.Cloud });
+        break;
+      default:
+        this.setState({ icon: this.weatherIcon.Cloud });
+    }
+  }
   getCelsius(temp) {
     let cell = Math.floor(temp - 273.15);
     return cell;
@@ -43,8 +76,8 @@ class App extends React.Component {
       temp_max: this.getCelsius(response.main.temp_max),
       temp_min: this.getCelsius(response.main.temp_min),
       description: response.weather[0].description,
-      icon: this.weatherIcon.Thunderstorm,
     });
+    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
   };
 
   render() {
