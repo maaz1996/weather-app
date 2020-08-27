0a4ad4b89ff88addc0ed5ed46a188287d495aa85
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-28 01:37:12 +0530

    hot-fix: icon function

--- src/App.js
+++ src/App.js
@@ -34,28 +34,28 @@ class App extends React.Component {
   get_WeatherIcon(icons, rangeID) {
     switch (true) {
       case rangeID >= 200 && rangeID <= 232:
-        this.setState({ icon: this.weatherIcon.Thunderstorm });
+        this.setState({ icons: this.weatherIcon.Thunderstorm });
         break;
       case rangeID >= 300 && rangeID <= 321:
-        this.setState({ icon: this.weatherIcon.Drizzle });
+        this.setState({ icons: this.weatherIcon.Drizzle });
         break;
       case rangeID >= 500 && rangeID <= 531:
-        this.setState({ icon: this.weatherIcon.Rain });
+        this.setState({ icons: this.weatherIcon.Rain });
         break;
       case rangeID >= 600 && rangeID <= 622:
-        this.setState({ icon: this.weatherIcon.Snow });
+        this.setState({ icons: this.weatherIcon.Snow });
         break;
       case rangeID >= 701 && rangeID <= 781:
-        this.setState({ icon: this.weatherIcon.Atmosphere });
+        this.setState({ icons: this.weatherIcon.Atmosphere });
         break;
       case rangeID === 800:
-        this.setState({ icon: this.weatherIcon.Clear });
+        this.setState({ icons: this.weatherIcon.Clear });
         break;
       case rangeID >= 801 && rangeID <= 804:
-        this.setState({ icon: this.weatherIcon.Cloud });
+        this.setState({ icons: this.weatherIcon.Cloud });
         break;
       default:
-        this.setState({ icon: this.weatherIcon.Cloud });
+        this.setState({ icons: this.weatherIcon.Cloud });
     }
   }
   getCelsius(temp) {
